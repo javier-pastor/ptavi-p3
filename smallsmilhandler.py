4103519b8afd08c8e8fa2fd76af485810df2ec08
Imac <imac@iMac-de-Imac.local>
2017-10-12 12:15:46 +0200

    Actualizando ejercicio

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -1,72 +1,41 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-
-from xml.sax import make_parser  # metodo
-from xml.sax.handler import ContentHandler  # clase
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
 
 
 class SmallSMILHandler(ContentHandler):
+    """
+        Clase que usamos como pequeno manejador Smil
+    """
 
     def __init__(self):
-        """
-        Constructor. Inicializamos las variables
-        """
-        self.root_layout = {}  # esto es para decir que info guardar
-        self.region = {}
-        self.img = {}
-        self.audio = {}
-        self.textstream = {}
-        self.list = list()
+        self.list = []
 
     def startElement(self, name, attrs):
         """
-        Método que se llama cuando se abre una etiqueta
+            Crea una lista con las etiquetas y atributos de un archivo smil
         """
-        if name == 'root-layout':
-            self.root_layout['width'] = attrs.get('width', "")
-            self.root_layout['height'] = attrs.get('height', "")
-            self.root_layout['bground_clr'] = attrs.get('background-color', "")
-            self.list.append("Etiqueta : " + name)
-            self.list.append("\n")
-            self.list.append("Atributos : " + str(self.root_layout))
-        elif name == 'region':
-            self.region['id'] = attrs.get('ud', "")
-            self.region['top'] = attrs.get('height', "")
-            self.region['bottom'] = attrs.get('bottom', "")
-            self.region['left'] = attrs.get('left', "")
-            self.region['right'] = attrs.get('right', "")
-            self.list.append("Etiqueta : " + name)
-            self.list.append("Atributos : " + str(self.region))
-        elif name == 'img':
-            self.img['src'] = attrs.get('src', "")
-            self.img['region'] = attrs.get('region', "")
-            self.img['begin'] = attrs.get('begin', "")
-            self.img['dur'] = attrs.get('dur', "")
-            self.list.append("Etiqueta : " + name)
-            self.list.append("Atributos : " + str(self.img))
-        elif name == 'audio':
-            self.audio['src'] = attrs.get('src', "")
-            self.audio['begin'] = attrs.get('begin', "")
-            self.audio['dur'] = attrs.get('dur', "")
-            self.list.append("Etiqueta : " + name)
-            self.list.append("Atributos : " + str(self.audio))
-        elif name == 'textstream':
-            self.textstream['src'] = attrs.get('src', "")
-            self.textstream['region'] = attrs.get('region', "")
-            self.list.append("Etiqueta : " + name)
-            self.list.append("Atributos : " + str(self.textstream))
+
+        tags_list = ['root-layout', 'region', 'img', 'audio', 'textstream']
+
+        if name in tags_list:
+            attrs_dicc = {}
+            for attr in attrs.keys():
+                attrs_dicc[str(attr)] = str(attrs.get(attr, ''))
+            self.list.append([name, attrs_dicc])
 
     def get_tags(self):
+        """
+            Devuelve la lista de etiquetas y atributos del archivos smil
+        """
         return self.list
 
-
 if __name__ == "__main__":
-    """
-    Programa principal
-    """
+
     parser = make_parser()
-    cHandler = SmallSMILHandler()
-    parser.setContentHandler(cHandler)
+    sHandler = SmallSMILHandler()
+    parser.setContentHandler(sHandler)
     parser.parse(open('karaoke.smil'))
-    print(cHandler.get_tags())
+    print(sHandler.get_tags())
